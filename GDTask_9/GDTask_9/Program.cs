1204b7d2f4321293bce5de58a145f94580c737ed
Lucky932 <andersandrenatosamp@mail.ru>
2023-04-23 21:49:54 +0300

    GDTask_9/

---
+++ GDTask_9/GDTask_9/Program.cs
@@ -0,0 +1,31 @@
+﻿static string ROT13(string t)
+{
+    string tout = "";
+    int j;
+
+    foreach (char c in t)
+    {
+        if (Char.IsLetter(c))
+        {
+            if (Char.IsUpper(c))
+            {
+                tout += (char)(((c - 'A') + 13) % 26 + 'A');
+            }
+            if (Char.IsLower(c))
+            {
+                tout += (char)(((c - 'a') + 13) % 26 + 'a');
+            }
+        }
+        else
+        {
+            tout += c;
+        }
+    }
+    return tout;
+}
+
+Console.Write("Введите текст: ");
+string text = Console.ReadLine();
+string outtext = ROT13(text);
+
+Console.Write($"Зашифрованный текст: {outtext}");
